2b3201725741f6ee11de48bb16dfb7059f6cc5cd
archanadharsini <archanadharsini@localhost>
2023-08-04 16:34:39 +0530

    final

--- preprogram/src/main/java/July29_Aug4/MoneyBank.java
+++ preprogram/src/main/java/July29_Aug4/MoneyBank.java
@@ -2,7 +2,7 @@ package July29_Aug4;
 /*
  * input int -> 4
  * output int 
- * 1, 1+1, 1+2...1+6 --> 1+2+3+4+6+7
+ * 1, 1+1, 1+2...1+6 --> 1+2+3+4+5+6+7
  * then 1+1, (1+1)+1, (1+2)+1,  .....(1+6)+1 
  * -> 2+3+4++5+6+7+8
  * -> 3+4+5+6+7+8+9
@@ -25,32 +25,44 @@ public class MoneyBank {
 	Assert.assertEquals(10, actual);
 	}
 	
+	@Test
+	public void test2() {
+	int actual = Bank(10);
+	Assert.assertEquals(37, actual);
+	}
+	
+	
+	
 	
 	public int Bank( int n)
 	{
-		int res=0 ;
-		int sum=0;
-		int sum1= 0;
+		int count =1;
+		int value =1;
+		int sum = 0;
+		int startValue = 1;
 		
-		for ( int i =1;i<=n;i++)
+		for ( int i = 1; i<=n;i++)
 		{
-			sum = sum +i;
+			
+			sum = sum + value++; // sum = 0 + 1 ; count = 1		
+								// sum = 1+2 -> 3; count =2
+								// sum = 3+3 ->6 ; count =3 
+			count ++;  
+			
+			if ( count > 7)
+			{
+				count = 1;
+				startValue++;
+				value = startValue;
+				
+			}
+			
+			
+			
+			
 			
 		}
-		
-		for ( int j = n ; j<=(n+1);j++)
-		{
-			sum1= sum1 + j ;
-		}
-		
-		//res = sum + sum + n + sum + 2n + sum + 3n+.......sum+(n+1)n
-		
-		
-		
-		
-		res = (n*sum) + sum1;
-				
-		return res;
+		return sum;
 		
 		
 	}
